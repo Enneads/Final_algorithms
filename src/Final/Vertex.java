c5556b4a74ad6b402141b1b009d9525cac9665ac
Enneads <lsycrs@gmail.com>
2018-05-06 10:14:23 -0500

    JavaDoc for Vertex and created SpreadingActivation algorithm

--- src/Final/Vertex.java
+++ src/Final/Vertex.java
@@ -1,40 +1,75 @@
 package Final;
 
+/**
+ *  The {@code Vertex} class represents a vertex with a <em>name</em> and a <em>weight</em>.
+ *  It supports the following primary operation: create a vertex. It also provides
+ *  methods for getting or setting the <em>key</em> and <em>weight</em>,
+ *  as well as checking if a vertex is equal to another object.
+ *  <p>
+ *  All operations take constant time (in the worst case).
+ *
+ *  @author Logan Stapleton
+ *  @author Abigail Poole
+ *  @author Shiyu Lin
+ */
 public class Vertex {
     private String key;
     private double weight;
 
     /**
-     * This class creates the vertexes in the graph.
+     * Constructor initializes a vertex with name key and specified weight.
+     *
      * @param key
      * @param weight
      */
-
     public Vertex(String key, double weight) {
         this.key = key;
         this.weight = weight;
     }
 
+    /**
+     * Overloaded constructor initializes a vertex with name key and weight 1.
+     *
+     * @param key
+     */
     public Vertex(String key) {
         this.key = key;
         this.weight = 0D;
     }
 
+    /**
+     * Returns the name of vertex {@code v}.
+     *
+     * @return the name of vertex {@code v}
+     */
     public String getKey() {
         return key;
     }
 
+    /**
+     * Returns the weight of vertex {@code v}.
+     *
+     * @return the weight of vertex {@code v}
+     */
     public double getWeight() {
         return weight;
     }
 
+    /**
+     * Sets the weight of vertex {@code v}.
+     *
+     * @param wt the weight of vertex {@code v}
+     */
     public void setWeight(double wt) {
         weight = wt;
     }
 
     @Override
     /**
-     * This method overrides equals. It checks if two Vertexes are the same and returns a boolean.
+     * This method overrides equals. It checks if two vertices are the same and returns a boolean.
+     *
+     * @param obj the object to check against {@code v}
+     * @returns true if {@code v} equals {@code obj}; false if not
      */
     public boolean equals(Object obj) {
         if (this == obj)
@@ -53,6 +88,11 @@ public class Vertex {
         return true;
     }
 
+    /**
+     * Returns a string representation of the vertex.
+     *
+     * @return the name of the vertex
+     */
     @Override
     public String toString() {
         return key;
