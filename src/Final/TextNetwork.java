8bf62d2cea2b2c3f2e25cb8ad7af352c85b1478f
lstaplet <lstaplet@macalester.edu>
2018-05-06 12:46:54 -0500

    changed edge to set of vertices

--- src/Final/TextNetwork.java
+++ src/Final/TextNetwork.java
@@ -61,9 +61,19 @@ public class TextNetwork {
         ReadFile file = new ReadFile(fileName);
         String[] text = file.OpenFile();
 
+        // parses through every line in text
         for (String line : text) {
+
+            String tempStr = "";
+
+            // make line uppercase
+            // for(int j = 0; j < line.length(); j++) {//start character loop
+            line = line.toUpperCase();
+
+            // an array of words in a line
             String[] wordList = cutString(line);
 
+            //
             for (String i : wordList) {
                 for (String j : wordList) {
                     Edge e = new Edge(i, j);
@@ -84,11 +94,54 @@ public class TextNetwork {
         return line.split("\\W+");
     }
 
-//    public void SpreadingActivation(int iterTime, String active){
-//        int degree = 0;
-//        double sum = 0;
-//        for (v: )
-//    }
+    public void SpreadingActivation(int iterTime, LinkedList<String> active) {
+        Map<Vertex, LinkedList<Edge>> network = graph.getGraphMap();
+
+        // Create an initial network where important vertices (those in active list)
+        // are assigned weight 1; every other vertex has weight 0.
+        for (Vertex v : network.keySet()) { // loops over every vertex in network
+            Iterator iter = active.iterator();
+            while (iter.hasNext()) { // loops over every word in active
+                String word = (String) iter.next();
+                if (v.getKey().equals(word)) { // checks if vertex key is in active
+                    v.setWeight(1D);
+                }
+            }
+        }
+
+
+        // loops through iterTime number of iterations
+        for (int i=0; i<iterTime; i++){
+
+            // single iteration
+            for (Vertex v : network.keySet()) {
+                int degree = 0;
+                double sum = 0;
+
+                LinkedList<Edge> edgelist = network.get(v);
+
+                Iterator iter2 = edgelist.iterator();
+                while (iter2.hasNext()){
+                    Edge e = (Edge)iter2.next();
+                    double edgeweight = e.getWeight();
+                    Vertex u = null;
+                    for (Vertex v2:e.getEdge()){
+                        if (!v2.equals(v)){
+                            u = v2;
+                        }
+                    }
+
+                    double nodeweight = u.getWeight();
+                    sum += nodeweight*Math.exp(-1/edgeweight);
+                    degree++;
+
+                    if(v.getWeight() != 1D){
+                        v.setWeight(sum/degree);
+                    }
+                }
+            }
+        }
+    }
 
 
     public static void main(String[] args) throws IOException {
