cf5156f43eb2c791d8b963a9aa0a7657edac9d3f
LoganStapleton <lstaplet@macalester.edu>
2018-05-08 12:49:50 +0000

    Javadoc

--- src/Final/TextGraph.java
+++ src/Final/TextGraph.java
@@ -6,29 +6,75 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
+
+/**
+ *  The {@code TextGraph} class represents an undirected, weighted graph that contains
+ *  words read in from a text passage. Two vertices (words) are connected if they both
+ *  occur in the same line in the passage. Also runs a spreading activation algorithm on
+ *  a text graph. Runs these with a sample text passage in the main method.
+ *  It supports the following primary operations: makeGraph and SpreadingActivation. It also
+ *  provides methods for cutting a line into words, removing the most common English words 
+ *  from a text passage, activating certain vertices before calling the spreading activation algo.
+ *  <p>
+ *
+ *  @author Logan Stapleton
+ *  @author Abigail Poole
+ *  @author Shiyu Lin
+ */
 public class TextGraph {
     EdgeWeightedGraph graph;
 
+    /**
+     * Constructor initializes an empty EdgeWeightedGraph to be filled with words from a text passage in the makeGraph() method.
+     */
     public TextGraph() {
         this.graph = new EdgeWeightedGraph(new LinkedList<Edge>(), new LinkedList<Vertex>());
     }
 
+    /**
+     * Overloaded constructor initializes a text graph from an already existing EdgeWeightedGraph {@code graph}
+     *
+     * @param graph an EdgeWeightedGraph to include into a text graph
+     */
     public TextGraph(EdgeWeightedGraph graph) {
         this.graph = graph;
     }
 
+    /**
+     * Returns the EdgeWeightedGraph in this TextGraph
+     *
+     * @return graph this EdgeWeightedGraph in the text graph
+     */
     public EdgeWeightedGraph getGraph() {
         return graph;
     }
 
+    /**
+     * Set the EdgeWeightedGraph in this TextGraph
+     *
+     * @param graph the EdgeWeightedGraph to set into text graph
+     */
     public void setGraph(EdgeWeightedGraph graph) {
         this.graph = graph;
     }
 
+    /**
+     * Returns an array of strings of words in line of text.
+     *
+     * @param line the string of the line to be split into words
+     * @return an array of strings of all the words in a line
+     */
     private String[] cutString(String line){
         return line.split("\\W+");
     }
 
+    /**
+     * Checks whether a string is in an array of strings
+     *
+     * @param str a string to be found
+     * @param arry an array of strings to check if {@code str} is in
+     * @return true if {@code str} is in {@code arry}, false if not.
+     */
     private boolean contains(String str, String[] arry) {
         boolean found = false;
         for (String s : arry) {
@@ -90,6 +136,13 @@ public class TextGraph {
         return cutString(str);
     }
 
+    /**
+     * Reads in a text file and updates this TextGraph with a network of words.
+     * Two words are connected if they lie in the same line.
+     * The edge-weight between words equals the number of lines the words share.
+     *
+     * @param fileName a string with the name of a file to be read into the text graph
+     */
     public void makeGraph(String fileName) throws IOException {
         ReadFile file = new ReadFile(fileName);
         String[] text = file.OpenFile();
@@ -119,6 +172,12 @@ public class TextGraph {
          }
      }// end makeGraph method
 
+    /**
+     * Activates vertices with names in the list {@code active},
+     * (where activating a vertex is setting the node-weight to 1).
+     *
+     * @param active a list of strings of the names of vertices to activate
+     */
     public void activate(LinkedList<String> active){
         // Set activated vertices' weight (those in active list) to 1.
         for (Vertex v : graph.getVertices()) { // loops over every vertex in graph
@@ -132,7 +191,15 @@ public class TextGraph {
         }
     }// end activate method
 
-
+    /**
+     * Runs a spreading activation algorithm on this text graph.
+     * Note: each iteration parses over every node in the graph and updates
+     *  the node-weight based on a function of the node's current weight, the
+     *  weights of its neighbor nodes, and the edge-weight of the edges connecting
+     *  to those neighbor nodes. Iterates as many times as specified.
+     *
+     * @param iterTime the number of iterations to run spreading algorithm
+     */
     public void SpreadingActivation(int iterTime) {
         EdgeWeightedGraph updatedGraph = graph.clone();
 
@@ -160,7 +227,13 @@ public class TextGraph {
         }
     }// end spreading activation method
 
-
+    /**
+     * Takes in a list of vertices and returns a new list of the same vertices,
+     * sorted by node-weight from heaviest to lightest. (Bubble sort).
+     *
+     * @param vertices a list of vertices to be sorted
+     * @return the list of vertices in order from highest node-weight to lowest
+     */
     public LinkedList<Vertex> getSortedVertices(LinkedList<Vertex> vertices){
         int loop = vertices.size();
         for(int i = 0; i < loop - 1; i++){
@@ -178,13 +251,26 @@ public class TextGraph {
         return vertices;
     }// end getSortedVertices method
 
+    /**
+     * Helper method to getSortedVertices that swaps the position of two vertices.
+     *
+     * @param list a list of vertices
+     * @param posiOne the position of one vertex
+     * @param posiTwo the position of the other vertex
+     */
     private void swapVer(LinkedList<Vertex> list, int posiOne, int posiTwo){
         Vertex tempVer = list.get(posiOne);
         list.set(posiOne,list.get(posiTwo));
         list.set(posiTwo,tempVer);
     }
 
-
+    /**
+     * The driver: creates a sample TextGraph out of a text file of posts from
+     * the anti-black forum chimpmania.com, activates the word "NIGGER", then runs
+     * a spreading activation algorithm on this graph. Outputs a sorted list of nodes
+     * and two files --chimpmaniaNodes.csv and chimpmaniaEdges.csv-- that may be 
+     * input into Gephi to create a network with this updated graph.
+     */
     public static void main(String[] args) throws IOException {
         String file_name = "Final/text_files/chimpmania.txt";
         ReadFile file = new ReadFile(file_name);
